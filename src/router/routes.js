8ca4efbe69778c352845b018188595db14e2531e
Mikiyas Belhu <mikiyasbelhu@gmail.com>
2020-04-02 14:30:11 -0500

    add router

---
+++ src/router/routes.js
@@ -0,0 +1,39 @@
+import { Login, ForgotPassword } from "@/modules/account";
+import AppLayout from "@/modules/layout";
+import NotFoundPage from "@/components/NotFoundPage.vue";
+
+import accountRoutes from "@/modules/account/account.routes";
+import homeRoutes from "../modules/home/home.routes";
+import manageRoutes from "../modules/manage/manage.routes";
+
+const routes = [
+  {
+    path: "/",
+    redirect: "/home"
+  },
+  {
+    path: "/home",
+    component: AppLayout,
+    children: [
+      ...accountRoutes,
+      ...homeRoutes,
+      ...manageRoutes
+    ]
+  },
+  {
+    path: "/login",
+    name: "login",
+    component: Login
+  },
+  {
+    path: "/forgot",
+    name: "forgot",
+    component: ForgotPassword
+  },
+  {
+    path: "*",
+    name: "not-found",
+    component: NotFoundPage
+  }
+];
+export default routes;
