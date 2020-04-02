4d85a8dbaedb19975598a823cab42873fe6eace5
Mikiyas Belhu <mikiyasbelhu@gmail.com>
2020-04-02 14:27:22 -0500

    add home module

---
+++ src/modules/layout/AppLayout.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-card class="overflow-hidden">
+    <nav-bar />
+    <v-sheet
+      id="scrolling-techniques-4"
+      class="overflow-y-auto"
+    >
+      <v-container style="height: 1000px; margin-top:225px">
+        <router-view />
+      </v-container>
+    </v-sheet>
+  </v-card>
+</template>
+
+<script>
+import NavBar from "./NavBar.vue";
+
+export default {
+  components: {
+    NavBar
+  }
+};
+</script>
